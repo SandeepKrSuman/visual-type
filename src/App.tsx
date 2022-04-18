b6bb648b2230a28cf293b8264f327bb01db64688
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-04-19 00:06:12 +0530

    modified App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Typer from "./components/Typer";
+import Visualizer from "./components/Visualizer";
+import './App.scss';
+import {generateWord} from './WordGenerator';
+
 function App() {
+  const [word, setWord] = useState<string>(generateWord());
+  const next = () => {
+    setWord(generateWord());
+  }
   return (
-    <div className="App">
-      <h1>Hello Typescript</h1>
+    <div className="binder">
+      <Visualizer toShow={word} />
+      <Typer toType={word} next={next} />
     </div>
   );
 }
